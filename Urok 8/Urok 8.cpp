9ad90ec3c4a49e253ae0e7aa43efde80ef6b0858
Enzowax <144791348+Enzowax@users.noreply.github.com>
2024-09-24 08:52:22 +0300

    Update Urok 8.cpp

--- Urok 8/Urok 8.cpp
+++ Urok 8/Urok 8.cpp
@@ -6,83 +6,83 @@ int main()
 {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
-	int a, b, c, s;
-	string d, f, g;
-	cout << "Выберите действие:\n 0 для соединения \n 1 - сложения \n 2 - умножения \n 3 - вычитания \n 4 - целого деления \n 5 - деления с остатком" << endl;
-	cin >> a;
+	int a1, b1, c1, s1;
+	string d1, f1, g1;
+	cout << "Выберите действие:\n 0 - соединения \n 1 - сложения \n 2 - умножения \n 3 - вычитания \n 4 - целого деления \n 5 - деления с остатком" << endl;
+	cin >> a1;
 	//cout << boolalpha;
-	if (a == 1)
+	if (a1 == 1)
 	{
 		cout << "Введите 1 число " << endl;
-		cin >> b;
+		cin >> b1;
 		cout << "Введите 2 число " << endl;
-		cin >> c;
-		s = b + c;
-		cout << "Сумма 2х чисел = " << s << endl;
+		cin >> c1;
+		s1 = b1 + c1;
+		cout << "Сумма двух чисел = " << s1 << endl;
 	}
 	else
-		if (a == 0)
+		if (a1 == 0)
 		{
 			cout << "Введите 1 слово" << endl;
-			cin >> d;
+			cin >> d1;
 			cout << "Введите 2 слово" << endl;
-			cin >> f;
-			g = d + " " + f;
-			cout << "Соединяем слова = " << g << endl;
+			cin >> f1;
+			g1 = d1 + " " + f1;
+			cout << "Соединяем слова = " << g1 << endl;
 		}
 		else
-			if (a == 2)
+			if (a1 == 2)
 			{
 				cout << "Введите 1 число" << endl;
-				cin >> b;
+				cin >> b1;
 				cout << "Введите 2 число" << endl;
-				cin >> c;
-				s = b * c;
-				cout << "Произведение 2х чисел  = " << s << endl;
+				cin >> c1;
+				s1 = b1 * c1;
+				cout << "Произведение 2х чисел  = " << s1 << endl;
 			}
 			else
-				if (a == 3)
+				if (a1 == 3)
 				{
 					cout << "Введите 1 число" << endl;
-					cin >> b;
+					cin >> b1;
 					cout << "Введите 2 число" << endl;
-					cin >> c;
-					if (b <= c)
+					cin >> c1;
+					if (b1 <= c1)
 					{
 						cout << "От меньшего нельзя отнять большее" << endl;
 					}
 					else
 					{
-						s = b - c;
-						cout << "Разность 2х чисел  = " << s << endl;
+						s1 = b1 - c1;
+						cout << "Разность 2х чисел  = " << s1 << endl;
 					}
 
 				}
 				else
-					if (a == 4)
+					if (a1 == 4)
 					{
 						cout << "Введите 1 число" << endl;
-						cin >> b;
+						cin >> b1;
 						cout << "Введите 2 число" << endl;
-						cin >> c;
-						s = b / c;
-						cout << "Частное 2х чисел  = " << s << endl;
+						cin >> c1;
+						s1 = b1 / c1;
+						cout << "Частное 2х чисел  = " << s1 << endl;
 					}
 					else
-						if (a == 5)
+						if (a1 == 5)
 						{
 							cout << "Введите 1 число" << endl;
-							cin >> b;
+							cin >> b1;
 							cout << "Введите 2 число" << endl;
-							cin >> c;
-							if (b <= c)
+							cin >> c1;
+							if (b1 <= c1)
 							{
 								cout << "Меньше не делится на большее" << endl;
 							}
 							else
 							{
-								s = b % c;
-								cout << "Остаток от деления  = " << s << endl;
+								s1 = b1 % c1;
+								cout << "Остаток от деления  = " << s1 << endl;
 							}
 						}
 						else
